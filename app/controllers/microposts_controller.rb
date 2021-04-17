7723dcf4cc936746aeeff3faa2021ea1f31e121d
Ali Dali <ali.dali01@stclairconnect.ca>
2021-04-16 20:53:45 -0400

    paginate

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -19,7 +19,8 @@ class MicropostsController < ApplicationController
   # GET /microposts
   # GET /microposts.json
   def index
-    @microposts = Micropost.all
+    # @microposts = Micropost.all
+    @microposts = Micropost.paginate(page: params[:page])
   end
 
   # GET /microposts/1
